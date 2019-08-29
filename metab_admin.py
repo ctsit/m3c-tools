6a953982109e47c8a09b94515c24c377a5fcdbf7
Hunter Jarrell <hjarrell555@gmail.com>
2019-08-29 12:32:24 -0400

    Add assoication page to metab admin.
    Add a page to associate a person with any of the different org types.

--- metab_admin.py
+++ metab_admin.py
@@ -46,6 +46,9 @@ def main_menu():
                 <div class="row my-3">
                     <button class="btn btn-info w-100" onclick="window.location.href = '/createperson'">Create New Person</button>
                 </div>
+                <div class="row my-3">
+                    <button class="btn btn-info w-100" onclick="window.location.href = '/associateperson'">Associate a Person with Inst/Dept/Lab</button>
+                </div>
             </div>
         </body>
     </html>
@@ -96,32 +99,32 @@ def upload_image():
                 {% endif %}
             {% endwith %}
 
-                <div class="form-group">
-                    <label>Search Display Name</label>
-                    <input id=searchInput class="form-control" list=displaynames name=displayname>
-                    <datalist id=displaynames>
-                        {% for name in dispNameList %}
-                            <option value="{{name}}">
-                        {% endfor %}
-                    </datalist>
-                </div>
+            <div class="form-group">
+                <label>Search Display Name</label>
+                <input id=searchInput class="form-control" list=displaynames name=displayname>
+                <datalist id=displaynames>
+                    {% for name in dispNameList %}
+                        <option value="{{name}}">
+                    {% endfor %}
+                </datalist>
+            </div>
 
             <form method=post enctype=multipart/form-data>
-                    <div class="form-group">
-                        <label>First Name</label>
-                        <input id=firstName class="form-control" type=text name=first_name>
-                    </div>
+                <div class="form-group">
+                    <label>First Name</label>
+                    <input id=firstName class="form-control" type=text name=first_name>
+                </div>
 
-                    <div class="form-group">
-                        <label>Last Name</label>
-                        <input id=lastName class="form-control" type=text name=last_name>
-                    </div>
+                <div class="form-group">
+                    <label>Last Name</label>
+                    <input id=lastName class="form-control" type=text name=last_name>
+                </div>
 
-                    <div class="form-group">
-                        <input type="file" class="form-control-file" id="inputGroupFile01" name=picture>
-                    </div>
+                <div class="form-group">
+                    <input type="file" class="form-control-file" id="inputGroupFile01" name=picture>
+                </div>
 
-                    <button class="btn btn-primary" type=submit>Upload</button>
+                <button class="btn btn-primary" type=submit>Upload</button>
             </form>
         </div>
         <script>
@@ -297,6 +300,177 @@ def create_person():
         </div>
     </body>
     ''', instituteList=institutes.keys(), departmentList=departments.keys(), labList=labs.keys())
+
+@app.route('/associateperson', methods=['GET', 'POST'])
+def associate_person():
+    display_names = []
+    cur = conn.cursor()
+
+    cur.execute('SELECT display_name, email, id from people;')
+    rows = cur.fetchall()
+    for row in rows:
+        display_names.append(row[0] + ' | ' + row[1] + ' | ' + str(row[2]))
+
+    institutes = {}
+    departments = {}
+    labs = {}
+
+    cur.execute('SELECT id, name, type from organizations')
+    for row in cur.fetchall():
+        if row[2] == 'institute':
+            institutes[row[1]] = row[0]
+        elif row[2] == 'department':
+            departments[row[1]] = row[0]
+        elif row[2] == 'lab':
+            labs[row[1]] = row[0]
+        else:
+            pass
+            # what do we do
+
+    cur.close()
+    if request.method == 'POST':
+        cur = conn.cursor()
+        try:
+            id = request.form['id'].strip()
+            institute = request.form['institute'].strip()
+            department = request.form['department'].strip()
+            lab = request.form['lab'].strip()
+
+            if id is '':
+                flash('Please search and select someone')
+                return redirect(request.url)
+            
+            if institute is '' and department is '' and lab is '':
+                flash('Please select or enter at least one institute, department, OR lab.')
+                return redirect(request.url)
+
+            if institute is not '':
+                if institute in institutes:
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (institutes[institute], id))
+                else:
+                    cur.execute('INSERT INTO organizations (name, type) VALUES (%s, %s) RETURNING id', (institute, 'institute'))
+                    org_id = cur.fetchone()[0]
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (org_id, id))
+
+            if department is not '':
+                if department in departments:
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (departments[department], id))
+                else:
+                    cur.execute('INSERT INTO organizations (name, type) VALUES (%s, %s) RETURNING id', (department, 'department'))
+                    org_id = cur.fetchone()[0]
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (org_id, id))
+            
+            if lab is not '':
+                if lab in labs:
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (labs[lab], id))
+                else:
+                    cur.execute('INSERT INTO organizations (name, type) VALUES (%s, %s) RETURNING id', (lab, 'lab'))
+                    org_id = cur.fetchone()[0]
+                    cur.execute('INSERT INTO associations (organization_id, person_id) VALUES (%s, %s)', (org_id, id))
+
+            conn.commit()
+            flash('Association created successfully')
+        except Exception as e:
+            print(e)
+            conn.rollback()
+            flash('Error creating association')
+        finally:
+            cur.close()
+        return redirect(request.url)
+
+    return render_template_string('''
+    <!doctype html>
+    <head>
+        <title>Create a new Person</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container mx-auto" style="width: 50%;">
+            <h1 class="mx-auto">Associate a Person with a Inst/Dept/Lab</h1>
+            <a href="/">Back to Home</a>
+            {% with messages = get_flashed_messages() %}
+                {% if messages %}
+                    {% for message in messages %}
+                        <div class="alert alert-warning" role="alert">
+                            {{ message }}
+                        </div>
+                    {% endfor %}
+                {% endif %}
+            {% endwith %}
+
+            <div class="form-group">
+                <label>Search Display Name</label>
+                <input id=searchInput class="form-control" list=displaynames name=displayname>
+                <datalist id=displaynames>
+                    {% for name in dispNameList %}
+                        <option value="{{name}}">
+                    {% endfor %}
+                </datalist>
+            </div>
+
+            <form method=post enctype=multipart/form-data>
+                <div class="form-group">
+                    <label>Name</label>
+                    <input readonly id=name class="form-control" type=text name=name>
+                </div>
+
+                <div class="form-group">
+                    <label>Email</label>
+                    <input readonly id=email class="form-control" type=text name=email>
+                </div>
+
+                <input hidden readonly id=id type=text name=id>
+
+                <div class="form-group">
+                    <label>Institute</label>
+                    <input class="form-control" list=institutes name=institute>
+                    <datalist id=institutes>
+                        {% for inst in instituteList %}
+                            <option value="{{inst}}">
+                        {% endfor %}
+                    </datalist>
+                </div>
+
+                <div class="form-group">
+                    <label>Department</label>
+                    <input class="form-control" list=departments name=department>
+                    <datalist id=departments>
+                        {% for dept in departmentList %}
+                            <option value="{{dept}}">
+                        {% endfor %}
+                    </datalist>
+                </div>
+
+                <div class="form-group">
+                    <label>Labs</label>
+                    <input class="form-control" list=labs name=lab>
+                    <datalist id=labs>
+                        {% for labItem in labList %}
+                            <option value="{{labItem}}">
+                        {% endfor %}
+                    </datalist>
+                </div>
+
+                <button class="btn btn-primary" type="submit">Associate Person</button>
+            </form>
+        </div>
+        <script>
+            const displayNameInput = document.getElementById('searchInput');
+            const displayNames = [...document.getElementById('displaynames').childNodes].filter(name => name.value).map(name => name.value);
+            const name = document.getElementById('name');
+            const email = document.getElementById('email');
+            const id = document.getElementById('id');
+            displayNameInput.addEventListener('change', (e) => {
+                if (displayNames.includes(e.srcElement.value)) {
+                    const splitName = e.srcElement.value.split('|');
+                    name.value = splitName[0];
+                    email.value = splitName[1];
+                    id.value = splitName[2];
+                }
+            });
+        </script>
+    </body>
+    ''', dispNameList=display_names, instituteList=institutes.keys(), departmentList=departments.keys(), labList=labs.keys())
 
 def main():
     '''Sets up a simple website for admin tasks'''
